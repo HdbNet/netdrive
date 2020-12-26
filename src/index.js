097c8847496867a214664904eaf25fb2c2ca8d61
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-26 18:45:43 +0000

    Update index file (index.js)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-let main = () => {
-    console.log ("Hello, World");
-}
+const express = require('express');
+const router  = require('./routes/router');
+const cors    = require('cors');
+const enoent  = require('./middlewares/enoent');
 
-main()
+
+const port     = process.env.PORT || 8000;
+const netdrive = express();
+
+
+netdrive.use(express.json());
+netdrive.use(cors());
+
+
+netdrive.use('/api', router);
+netdrive.use(enoent);
+
+
+netdrive.listen(port, () => {
+    console.log('Server running on port', port)
+});
